cbf60d7549a289ba88a19357239418b3e5759f81
BAlirezaei <b.alirezaei@gmail.com>
2021-01-20 21:56:47 +0330

    merge conflict

---
+++ MozaeekCore.Mapper/Request/UnProcessedRequestProfile.cs
@@ -0,0 +1,15 @@
+﻿using MozaeekCore.ApplicationService.Contract;
+using MozaeekCore.Domain;
+
+namespace MozaeekCore.Mapper.Request
+{
+  
+    public static class UnProcessedRequestMapping
+    {
+        public static UnProcessedRequestDto GetOperatorsSelectList(this UnProcessedRequest domain)
+        {
+            var mapper = new AutoMapper.Mapper(new MapperConfiguration(v => { }));
+            return mapper.Map<UnProcessedRequestDto>(domain);
+        }
+    }
+}
