755f1532491d574d74f866753533f9e84ea318d4
Bahar Alirezaei <b.alirezaei@gmail.com>
2021-07-12 11:55:55 +0430

    Delete MozaeekCore.ApplicationService.Contract directory

--- MozaeekCore.ApplicationService.Contract/CurrentUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MozaeekCore.ApplicationService.Contract
-{
-    public class CurrentUser
-    {
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-
-    }
-}
--- MozaeekCore.ApplicationService.Contract/UnProcessedRequest/UnProcessedRequestCommandResult.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MozaeekCore.ApplicationService.Contract
-{
-    public class UnProcessedRequestCommandResult
-    {
-        public long Id { get; set; }
-    }
-}
